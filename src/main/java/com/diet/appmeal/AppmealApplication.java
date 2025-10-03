2b67ada54de16c56c7f6b84e18ce525b5590f370
Andrea Cassitta <andreacassitta@MacBook-Air-di-Andrea.local>
2025-10-03 21:22:52 +0200

    first commit

---
+++ src/main/java/com/diet/appmeal/AppmealApplication.java
@@ -0,0 +1,13 @@
+package com.diet.appmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diet/appmeal/AppmealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diet.appmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
