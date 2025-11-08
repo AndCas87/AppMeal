c6fbc200cc887b8dbda34cc85231163fcc8e3489
andcas <andreacassitta@hotmail.it>
2025-11-08 18:20:42 +0100

    RequestParam

--- src/main/java/com/diet/appmeal/controllers/IndexController.java
+++ src/main/java/com/diet/appmeal/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.diet.appmeal.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -18,8 +19,8 @@ public class IndexController {
     }
 
     @GetMapping("index") // root webapp
-    public String getWelcome2(Model model) {
-        model.addAttribute("intestazione", "Benvenuti nell'index page");
+    public String getWelcome2(Model model,@RequestParam("name") String name) {
+        model.addAttribute("intestazione", String.format( "Benvenuto %s nell'index page",name));
         model.addAttribute("saluti", saluti);
 
         return "index";
